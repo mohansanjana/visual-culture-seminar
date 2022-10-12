7bce8d1e08fb6085b6705aecab3df4e7829216d3
mohansanjana <mohas206@newschool.edu>
2022-10-12 15:03:37 -0400

    like button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#heart").tap(function(){
+    $("#heart").click(function(){
       if($("#heart").hasClass("liked")){
         $("#heart").html('<i class="fa fa-heart-o" aria-hidden="true"></i>');
         $("#heart").removeClass("liked");
